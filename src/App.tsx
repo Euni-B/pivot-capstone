269ee716450545b4f09a73a6409b7bb0eaedb0ed
Eunice <ebontreger@gmail.com>
2026-06-13 05:17:01 -0700

    final dependency installations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="text-5xl font-bold text-blue-500">
       Tailwind is working! Hey Love!!
